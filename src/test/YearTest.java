9b8bfef2c3719939dc6dcc56c2ddedbecc2554f7
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2025-04-18 03:13:16 +0200

    test: compareTo method

--- src/test/YearTest.java
+++ src/test/YearTest.java
@@ -3,7 +3,10 @@ package test;
 import org.jfree.data.time.Year;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Calendar;
+
+// get all assertions
+import static org.junit.Assert.*;
 
 public class YearTest {
     Year year;
@@ -15,5 +18,20 @@ public class YearTest {
     public void testYearDefaultCtor() {
         arrange();
         assertEquals(2025, year.getYear());
+    }
+
+
+    @Test
+    public void compareTo_SHOULD_returnNegative() {
+        Year year1 = new Year(2024);
+        Year year2 = new Year(2025);
+        assertEquals(-1, year1.compareTo(year2));
+    }
+
+
+    @Test public void compareTo_SHOULD_returnPositive() {
+        Year year1 = new Year(2026);
+        Year year2 = new Year(2025);
+        assertEquals(1, year1.compareTo(year2));
     }
 }
